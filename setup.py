efe4c94f33deaa46b787e8fdfe2e3572eb7956a6
CP6 <number1dh@gmail.com>
2022-06-05 05:10:38 +0900

    Feat: Consolidate packages with enunu_kor_tool and add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from glob import glob
+from posixpath import basename, splitext
+import setuptools
+
+g2p4utau_required_packages = [
+    "jamo==0.4.1",
+    "regex",
+    "g2pk==0.9.3",
+]
+
+total_required_packages = g2p4utau_required_packages
+
+setuptools.setup(
+    name="enunu-kor-tool",
+    version="0.0.4",
+    author="cardroid",
+    author_email="carbonsindh@gmail.com",
+    description="enunu Korean language support script collection",
+    install_requires=total_required_packages,
+    license="MIT",
+    packages=setuptools.find_packages(where="enunu-kor-tool"),
+    package_dir={"": "enunu-kor-tool"},
+    python_requires=">=3.8",
+    include_package_data=True,
+    entry_points={"console_scripts": ["g2pk4utau=g2pk4utau.g2pk4utau:main"]},
+)
