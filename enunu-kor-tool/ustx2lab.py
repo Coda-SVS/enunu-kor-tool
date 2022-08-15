b319b9cba3e70967f247a87f9e2bafd157fed35c
Cardroid <number1dh@gmail.com>
2022-08-16 03:45:53 +0900

    Fix: Phoneme conversion error

--- enunu-kor-tool/ustx2lab.py
+++ enunu-kor-tool/ustx2lab.py
@@ -9,6 +9,9 @@ import g2pk4utau
 from utaupyk._ustx2ust import Ustx2Ust_Converter
 
 USE_G2PK4UTAU = True
+USE_TIMELINE = True
+
+g2p_converter = None
 
 
 def convert(input_filepath: str, d_table: dict, g2p_converter: Union[g2pk4utau.g2pk4utau, None] = None):
@@ -24,27 +27,30 @@ def convert(input_filepath: str, d_table: dict, g2p_converter: Union[g2pk4utau.g
     global_length = 0
 
     phonemes = []
-    for idx in range(notes_len := len(ust.notes)):
+    for idx in (phn_tqdm := tqdm(range(notes_len := len(ust.notes)), leave=False)):
         note = ust.notes[idx]
+        phn_tqdm.set_description(f"Lyric = {note.lyric}")
 
         if g2p_converter != None and g2pk4utau.isHangul(note.lyric):
-            if idx == 0:
-                note_block = ("", note, ust.notes[idx + 1])
-            elif idx == notes_len - 1:
-                note_block = (ust.notes[idx - 1], note, "")
-            else:
-                note_block = (ust.notes[idx - 1], note, ust.notes[idx + 1])
+            prev_note = ust.notes[idx - 1] if idx != 0 else ""
+            next_note = ust.notes[idx + 1] if idx + 1 < notes_len else ""
+
+            note_block = (prev_note, note, next_note)
+
+            current_phn_idx = 1
 
             orginal_lyrics = []
             for idx in range(3):
                 if isinstance(note_block[idx], utaupy.ust.Note):
-                    if idx != 2 or g2pk4utau.isHangul(note_block[idx].lyric):
+                    if g2pk4utau.isHangul(note_block[idx].lyric):
                         orginal_lyrics.append(note_block[idx].lyric)
+                    elif idx == 0:
+                        current_phn_idx = 0
 
             kor_phn_result = g2p_converter("".join(orginal_lyrics))
             kor_phn_token = kor_phn_result[2]
 
-            phns: List[str] = kor_phn_token[1].split(" ")
+            phns: List[str] = kor_phn_token[current_phn_idx].split(" ")
 
             phn_time_length = length100ns_converter(note) / len(phns)
 
@@ -52,7 +58,7 @@ def convert(input_filepath: str, d_table: dict, g2p_converter: Union[g2pk4utau.g
                 phonemes.append((round(global_length), phn))
                 global_length += phn_time_length
         else:
-            orginal_lyrics = note.lyric
+            orginal_lyrics = note.lyric.split(" ")
 
             phn_time_length = length100ns_converter(note) / len(orginal_lyrics)
 
@@ -76,6 +82,8 @@ def convert(input_filepath: str, d_table: dict, g2p_converter: Union[g2pk4utau.g
 
 
 def ustx2lab(table_filepath: str, input_filepath: str, output_dirpath: str):
+    global USE_G2PK4UTAU, USE_TIMELINE, g2p_converter
+
     input_filepath, output_dirpath = os.path.realpath(input_filepath), os.path.realpath(output_dirpath)
 
     if (filepath_without_ext := os.path.splitext(input_filepath))[1] == ".ustx":
@@ -84,41 +92,60 @@ def ustx2lab(table_filepath: str, input_filepath: str, output_dirpath: str):
         converter.save_ust(input_filepath)
 
     d_table = utaupy.table.load(table_filepath, encoding="utf-8")
-    g2p_converter = g2pk4utau.g2pk4utau() if USE_G2PK4UTAU else None
+    if USE_G2PK4UTAU and g2p_converter == None:
+        g2p_converter = g2pk4utau.g2pk4utau()
 
     lab = convert(input_filepath, d_table, g2p_converter)
 
     for idx, line in enumerate(lab):
         start, end, phn = line
-        lab[idx] = f"{start} {end} {phn}"
+        if USE_TIMELINE:
+            lab[idx] = f"{start} {end} {phn}"
+        else:
+            lab[idx] = phn
 
     os.makedirs(output_dirpath, exist_ok=True)
 
     with open(os.path.join(output_dirpath, os.path.splitext(os.path.basename(input_filepath))[0] + ".lab"), "w", encoding="utf-8") as f:
-        f.write("\n".join(lab))
+        f.write("\n".join(lab) + "\n")
 
 
 def main():
     import argparse
 
-    global USE_G2PK4UTAU
+    global USE_G2PK4UTAU, USE_TIMELINE
 
     parser = argparse.ArgumentParser(description="UTAU 프로젝트 파일에서 라벨을 추론합니다.")
 
     parser.add_argument("-d", dest="table", required=True, help="table 파일 경로")
     parser.add_argument("-i", dest="input", required=True, help="단일 Ust 또는 Ustx 파일, 또는 해당 파일이 모여있는 디렉토리 경로")
     parser.add_argument("-o", dest="output", required=True, help="출력 디렉토리 경로")
+    parser.add_argument("--no-time", dest="notuse_timeline", action="store_true", help="시간을 출력하지 않습니다.")
     parser.add_argument("--no-g2p", dest="notuse_g2pk4utau", action="store_true", help="g2pk4utau를 사용하지 않습니다.")
 
     args = vars(parser.parse_args())
 
     USE_G2PK4UTAU = not args["notuse_g2pk4utau"]
+    USE_TIMELINE = not args["notuse_timeline"]
 
     input_files = []
-    input_files.extend(glob(os.path.join(args["input"], "*.ust")))
     input_files.extend(glob(os.path.join(args["input"], "*.ustx")))
+    input_files.extend(glob(os.path.join(args["input"], "*.ust")))
+
+    # Duplicate input file filter
+    templist = []
+    filename_filter = []
+    for file_fullname in input_files:
+        if not (filename := os.path.splitext(os.path.basename(file_fullname))[0]).endswith("-autosave") and filename not in filename_filter:
+            filename_filter.append(filename)
+            templist.append(file_fullname)
+    input_files = templist
 
     for input_filepath in tqdm(input_files, desc="Processing..."):
         ustx2lab(table_filepath=args["table"], input_filepath=input_filepath, output_dirpath=args["output"])
 
     print("done.")
+
+
+if __name__ == "__main__":
+    main()
