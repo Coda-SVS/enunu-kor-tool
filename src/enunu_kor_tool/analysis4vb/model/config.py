a8386ae82b575353f7a05ed716fdc73dd28b1db4
Cardroid <number1dh@gmail.com>
2022-08-27 11:30:01 +0900

    Fix: Config 파일의 옵션 누락 방지

--- src/enunu_kor_tool/analysis4vb/model/config.py
+++ src/enunu_kor_tool/analysis4vb/model/config.py
@@ -1,11 +1,13 @@
 from typing import Dict, List
 
 from enunu_kor_tool.abc import DictBase
+from enunu_kor_tool.analysis4vb.config import DEFAULT_CONFIG
 from enunu_kor_tool.analysis4vb.model.config_phonemes import DB_Config_Phonemes
 
 
 class DB_Config(DictBase):
     def __init__(self, config: Dict) -> None:
+        self._data.update(DEFAULT_CONFIG)
         self._data.update(config)
         self.__config_phonemes = DB_Config_Phonemes(config["phonemes"])
 
