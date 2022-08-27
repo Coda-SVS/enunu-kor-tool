7712c95145c8d0b429a8a47395866f279dfeb5f8
Cardroid <number1dh@gmail.com>
2022-08-27 13:26:20 +0900

    Feat: lab 분석기 개선

--- src/enunu_kor_tool/analysis4vb/functions/lab.py
+++ src/enunu_kor_tool/analysis4vb/functions/lab.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Dict
 
 from tqdm import tqdm
 
@@ -9,23 +10,32 @@ from enunu_kor_tool.analysis4vb.model import DB_Info
 def __preprocess(func):
     def preprocess_wrapper(db_info: DB_Info, logger: logging.Logger):
         if "labs" not in db_info.cache:
-            __lab_loader(db_info, log.get_logger("lab_loader", db_info.config.options["log_level"]))
+            lab_loader_logger = log.get_logger("lab_loader", db_info.config.options["log_level"])
+            is_exist_error = __lab_loader(db_info, lab_loader_logger)
+            if is_exist_error:
+                lab_loader_logger.warning("lab 파일을 로드하는 중 오류가 발견되었습니다. 이후 작업에 영향을 끼칠 수 있습니다.")
+
         return func(db_info, logger)
 
     return preprocess_wrapper
 
 
-def __lab_loader(db_info: DB_Info, logger: logging.Logger):
+def __lab_loader(db_info: DB_Info, logger: logging.Logger) -> bool:
     """
     lab 파일을 로드 및 검사합니다.
-    cache.labs 에 lab 파일들을 로드하여 저장합니다.
+
+    cache.labs에 lab 파일들을 로드하여 저장합니다.
+
+    Returns:
+        bool: 오류의 존재 여부
     """
 
     phonemes_files = db_info.files.lab
     encoding = db_info.config.options.get("encoding", "utf-8")
-    line_no_formatter = lambda ln: str(ln).rjust(4)
+    line_num_formatter = lambda ln: str(ln).rjust(4)
 
-    lab_list = []
+    error_flag = False
+    labs = {}
     lab_global_line_count = 0
     for file in (file_tqdm := tqdm(phonemes_files, leave=False)):
         file_tqdm.set_description(f"Processing... [{file}]")
@@ -40,7 +50,7 @@ def __lab_loader(db_info: DB_Info, logger: logging.Logger):
                 line_pieces = list(filter(lambda p: p not in ["\n", ""] and not p.isspace(), line.split(" ")))
                 if len(line_pieces) <= 2:
                     p_line = line.replace("\n", "\\n")
-                    logger.warning(f'[Line {line_no_formatter(idx)}] 파싱할 수 없는 라인을 건너뛰었습니다. [Content: "{p_line}"]')
+                    logger.warning(f'[Line {line_num_formatter(idx)}] 파싱할 수 없는 라인을 건너뛰었습니다. [Content: "{p_line}"]')
                     error_line_count += 1
                     continue
                 start, end, *phn = line_pieces
@@ -49,20 +59,37 @@ def __lab_loader(db_info: DB_Info, logger: logging.Logger):
         length = lab[-1][1]
         lab_len = len(lab)
         lab_global_line_count += lab_len
-        logger.info(f"lab 파일을 로드했습니다. [총 라인 수: {line_no_formatter(lab_len)}] [길이: {round(length / 10000000, 1)}s ({length} 100ns)] [오류 라인 수: {error_line_count}]")
+        logger.info(f"lab 파일을 로드했습니다. [총 라인 수: {line_num_formatter(lab_len)}] [길이: {round(length / 10000000, 1)}s ({length} 100ns)] [오류 라인 수: {error_line_count}]")
 
         logger.debug(f"오류 검사 중...")
         global_length = 0
         for idx, (start, end, phn) in enumerate(lab, 1):
             if phn not in db_info.config.phonemes.all:
-                logger.warning(f"[Line {line_no_formatter(idx)}] [{phn}] Config에 명시되지 않은 음소가 사용되었습니다.")
+                logger.warning(f"[Line {line_num_formatter(idx)}] [{phn}] Config에 명시되지 않은 음소가 사용되었습니다.")
+                error_line_count += 1
             if start >= end:
-                logger.warning(f"[Line {line_no_formatter(idx)}] 종료시점이 시작지점보다 빠릅니다.")
+                logger.warning(f"[Line {line_num_formatter(idx)}] 종료시점이 시작지점보다 빠릅니다.")
+                error_line_count += 1
             if global_length != start:
-                logger.warning(f"[Line {line_no_formatter(idx)}] 시작시점이 이전 종료지점과 다릅니다.")
+                logger.warning(f"[Line {line_num_formatter(idx)}] 시작시점이 이전 종료지점과 다릅니다.")
+                error_line_count += 1
 
             global_length = end
 
-        lab_list.append(lab)
-    logger.info(f"모든 lab 파일을 로드했습니다. [lab 파일 수: {len(lab_list)}] [총 라인 수: {lab_global_line_count}]")
-    db_info.cache["labs"] = lab_list
+        if error_line_count > 0:
+            logger.warning(f"총 [{line_num_formatter(error_line_count)}] 개의 오류가 발견되었습니다.")
+            error_flag = True
+
+        labs[file] = lab
+    logger.info(f"모든 lab 파일을 로드했습니다. [lab 파일 수: {len(labs)}] [총 라인 수: {lab_global_line_count}]")
+    db_info.cache["labs"] = labs
+
+    return error_flag
+
+
+@__preprocess
+def lab_error_check(db_info: DB_Info, logger: logging.Logger):
+    # 이미 로딩 과정에서 검사가 이루어지므로, 이 함수는 더미 함수임.
+    logger.info("검사 완료.")
+    pass
+
